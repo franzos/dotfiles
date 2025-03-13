87e30c943a1a8883e440d23d558dcf079db0f22c
Franz Geffke <m@f-a.nz>
2025-03-13 15:06:24 +0000

    added system and desktop channels.scm

---
+++ system.channels.scm
@@ -0,0 +1,11 @@
+(cons* (channel
+        (name 'pantherx)
+        (branch "master")
+        (url "https://channels.pantherx.org/git/panther.git")
+         (introduction
+          (make-channel-introduction
+           "54b4056ac571611892c743b65f4c47dc298c49da"
+           (openpgp-fingerprint
+            "A36A D41E ECC7 A871 1003  5D24 524F EB1A 9D33 C9CB"))))
+       %default-channels)
+
